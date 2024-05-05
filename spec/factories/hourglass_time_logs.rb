d60be8bc281198deef0ecc200a8cbab929cacba1
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2024-05-05 18:03:09 +0200

    fix: avoid randomly failing tests

--- spec/factories/hourglass_time_logs.rb
+++ spec/factories/hourglass_time_logs.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :time_log, class: 'Hourglass::TimeLog' do
     user
     start { faker_between Date.today, Date.today, :morning }
-    stop { faker_between Date.today, Date.today, :afternoon }
+    stop { faker_between Date.today, Date.today, :evening }
     factory :time_log_with_comments do
       comments { Faker::Hacker.say_something_smart }
     end
